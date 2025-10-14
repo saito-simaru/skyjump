0309689465d5901a717047a3fecb346cc0a08e07
vibio <visbio.tamauniv@gmail.com>
2025-10-14 17:09:42 +0900

    プレイヤーの横移動を作成

--- Assets/script/move.cs
+++ Assets/script/move.cs
@@ -1,8 +1,11 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class move : MonoBehaviour
 {
+    private int movepositonindex = 2;
+    
     //プレイヤーの移動できるポイントを５つ設定
     Vector3[] movepositions = new Vector3[]
     {
@@ -13,8 +16,40 @@ public class move : MonoBehaviour
         new Vector3(-0.35f, 0.6f, -0.4f)
     };
 
-    public void OnMove()
+    void Start()
     {
-        Debug.Log("横移動を開始");
+        gameObject.transform.localPosition = movepositions[2];
+    }
+
+
+
+    public void Leftmove(InputAction.CallbackContext context)
+    {
+
+        if (context.performed)
+        {
+            Debug.Log("Left");
+            if (movepositonindex > 0)
+            {
+                movepositonindex -= 1;
+            }
+
+            gameObject.transform.localPosition = movepositions[movepositonindex];
+        }
+
+    }
+    public void Rightmove(InputAction.CallbackContext context)
+    {
+        Debug.Log("i");
+        if (context.performed)
+        {
+            Debug.Log("right");
+            if (movepositonindex < 4)
+            {
+                movepositonindex += 1;
+            }
+
+            gameObject.transform.localPosition = movepositions[movepositonindex];
+        }
     }
 }
